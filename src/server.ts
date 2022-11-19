5cde0fbabb2ee9197b22b66b054aaa20792317c9
Stephen Dickinson <stephencottontail@me.com>
2022-11-19 15:30:08 -0700

    WIP: handling slug in URL
    Couldn't find a solution that satisfies me:
    
    * Must not break the browser back/forward buttons
    * Must be able to link to a particular note
    	* Ok if the 'stack' is lost, though
    * Clicking on a previous note in the stack brings it forward
    	* Shouldn't break back/forward though

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express, { Express, Request, Response } from 'express';
+import pug from 'pug';
+import url from 'node:url';
 import dotenv from 'dotenv';
 
 const app: Express = express();
@@ -8,13 +10,17 @@ app.set( 'views', './src/views' );
 dotenv.config();
 const port = process.env.PORT || 3000;
 
-app.get( '/', ( req: Request, res: Response ) => {
-	res.render( 'index' );
+app.get( '/:slug', ( req: Request, res: Response ) => {
+	res.render( 'pane', { slug: req.params.slug, count: req.query.count }  );
 } );
 
-app.get( '/click', ( req: Request, res: Response ) => {
-	console.log( req.query );
-	res.render( 'pane', { count: req.query.count }  );
+app.get( '/', ( req: Request, res: Response ) => {
+	res.render( 'index' );
+	console.log( url.format( {
+		protocol: req.protocol,
+		host: req.get( 'host' ),
+		pathname: req.originalUrl
+	} ) );
 } );
 
 app.listen( port, () => {
