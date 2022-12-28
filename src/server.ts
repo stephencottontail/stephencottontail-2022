adf9c8d27e1c7806c3c4554a5516f2ab36db6fc2
Stephen Dickinson <stephencottontail@me.com>
2022-12-27 19:57:38 -0700

    Show complete HTML page when `slug` is present

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,41 @@
 import express, { Express, Request, Response } from 'express';
 import pug from 'pug';
 import url from 'node:url';
+import fs from 'node:fs';
 import dotenv from 'dotenv';
 
 const app: Express = express();
 app.set( 'view engine', 'pug' );
-app.set( 'views', './src/views' );
 
 dotenv.config();
 const port = process.env.PORT || 3000;
 
+const renderPage = ( options: object ): string => {
+	const page = pug.compileFile( './src/views/templates/content.pug' );
+	const result = page( options );
+
+	return result;
+}
+
 app.get( '/:slug', ( req: Request, res: Response ) => {
-	res.render( 'pane', { slug: req.params.slug, count: req.query.count } );
+	const count = req.query.count || 1;
+	const slug = req.params.slug;
+
+	const rendered = renderPage( {
+		count: count,
+		slug: slug
+	} );
+
+	res.send( rendered );
 } );
 
 app.get( '/', ( req: Request, res: Response ) => {
-	res.render( 'index' );
+	const rendered = renderPage( {
+		count: 1,
+		slug: 'none'
+	} );
+
+	res.send( rendered );
 } );
 
 app.listen( port, () => {
